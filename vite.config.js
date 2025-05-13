4ac1c042d385592f1b385a2c149c64cff98c2cb0
Sushila <143934893+sushrs@users.noreply.github.com>
2025-05-13 11:28:36 +0530

    Fix Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,9 +13,7 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     rollupOptions: {
-      input: {
-        main: './index.html'
-      }
+      input: resolve(__dirname, 'index.html')
     }
   }
 })
